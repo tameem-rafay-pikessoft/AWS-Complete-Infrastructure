9c7bf51139c7396005caa9e87eeb2f1db9218dbb
tameem-rafay <tameem.rafay@pikessoft.com>
2024-05-09 15:24:55 +0500

    update the varilables

--- module/ec2_instance_module/variables.tf
+++ module/ec2_instance_module/variables.tf
@@ -42,6 +42,21 @@ variable "ec2_instance_pem_file_name" {
   description = ".pem file name for EC2 instance"
 }
 
+variable "ssh_allowed_ip" {
+  type        = string
+  description = "IP address allowed for SSH (e.g., '1.2.3.4/32')"
+  validation {
+    condition     = var.ssh_allowed_ip != "0.0.0.0/0"
+    error_message = "SSH port must be specified and cannot be 0"
+  }
+}
+
+variable "security_group_allowed_ports" {
+  type        = list(number)
+  description = "List of ports to allow in the security group"
+  default     = [80, 443]
+}
+
 
 variable "tags" {
   type    = map(string)
